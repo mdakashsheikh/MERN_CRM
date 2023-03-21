63e4787d6a4a8222365212728f2f861e4e5356b6
MdAkashSheikh <nmakashsheikh@gmail.com>
2023-03-21 16:24:20 +0600

    image-resize

--- Nitto_Clint_Side-main/src/Components/BasicForm.jsx
+++ Nitto_Clint_Side-main/src/Components/BasicForm.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react';
-import axios from 'axios'
-import { Card } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-
+import axios from "axios";
+import { useState } from "react";
+import { Card } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 
 export default function BasicForm() {
-
-
   const [formName, setFormName] = useState("");
   const [formPhone, setFormPhone] = useState("");
   const [formEmail, setFormEmail] = useState("");
@@ -19,9 +16,7 @@ export default function BasicForm() {
   const [formInterestLebel, setFormInterestLebel] = useState("");
   const [newUser, setNewUser] = useState("");
 
-
   async function handleSubmit(e) {
-    
     e.preventDefault();
 
     console.log(formName);
@@ -30,65 +25,63 @@ export default function BasicForm() {
     console.log(formDesignation);
     console.log(formCompany);
     console.log(formInfo);
-    console.log(formInterestLebel)
-    
+    console.log(formInterestLebel);
+
     try {
-
       const formData = new FormData();
-        formData.append('formName', formName)
-        formData.append('formPhone', formPhone)
-        formData.append('formEmail', formEmail)
-        formData.append('formDesignation', formDesignation)
-        formData.append('formCompany', formCompany)
-        formData.append('formInfo', formInfo)
-        formData.append('formInterestLebel', formInterestLebel)
-        formData.append('photo', newUser.photo)
-        
-        const result = await axios.post('http://localhost:5000/post', formData)
-        console.log(result.data)
+      formData.append("formName", formName);
+      formData.append("formPhone", formPhone);
+      formData.append("formEmail", formEmail);
+      formData.append("formDesignation", formDesignation);
+      formData.append("formCompany", formCompany);
+      formData.append("formInfo", formInfo);
+      formData.append("formInterestLebel", formInterestLebel);
+      formData.append("photo", newUser.photo);
 
-        setFormName("");
-        setFormPhone("");
-        setFormEmail("");
-        setFormDesignation("");
-        setFormCompany("");
-        setFormInfo("");
-        setFormInterestLebel("")
-        console.log('Success'); 
-    
-      } catch (err) {
+      const result = await axios.post("http://localhost:5000/post", formData);
+      console.log(result.data);
+
+      setFormName("");
+      setFormPhone("");
+      setFormEmail("");
+      setFormDesignation("");
+      setFormCompany("");
+      setFormInfo("");
+      setFormInterestLebel("");
+      console.log("Success");
+    } catch (err) {
       console.log(err);
     }
   }
 
-  const handlePhoto=(e)=> {
-    setNewUser({...newUser, photo: e.target.files[0]})
-  }
-  
+  const handlePhoto = (e) => {
+    setNewUser({ ...newUser, photo: e.target.files[0] });
+  };
 
   return (
-      <Form onSubmit={(e)=> handleSubmit(e)} >
-      <div className='row justify-content-center'>
-        <div className='col-4'>
+    <Form onSubmit={(e) => handleSubmit(e)}>
+      <div className="row justify-content-center">
+        <div className="col-4">
           <h1>Customer List</h1>
         </div>
       </div>
 
       <Card>
-
         <Row className="my-3 mx-auto">
           <Form.Group as={Col} controlId="formName">
             <Form.Label>Name:</Form.Label>
-            <Form.Control placeholder="Enter customer name" 
-              onChange={(e)=> setFormName(e.target.value)}
+            <Form.Control
+              placeholder="Enter customer name"
+              onChange={(e) => setFormName(e.target.value)}
               value={formName}
             />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formPhone">
             <Form.Label>Phone</Form.Label>
-            <Form.Control placeholder="Enter Phone number" 
-              onChange={(e)=> setFormPhone(e.target.value)}
+            <Form.Control
+              placeholder="Enter Phone number"
+              onChange={(e) => setFormPhone(e.target.value)}
               value={formPhone}
             />
           </Form.Group>
@@ -97,35 +90,39 @@ export default function BasicForm() {
         <Row className="my-3 mx-auto">
           <Form.Group as={Col} controlId="formEmail">
             <Form.Label>Email:</Form.Label>
-            <Form.Control type='email' placeholder="Enter email" 
-              onChange={(e)=> setFormEmail(e.target.value)}
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) => setFormEmail(e.target.value)}
               value={formEmail}
             />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formDesignation">
             <Form.Label>Designation</Form.Label>
-            <Form.Control placeholder="Enter Designation" 
-              onChange={(e)=> setFormDesignation(e.target.value)}
+            <Form.Control
+              placeholder="Enter Designation"
+              onChange={(e) => setFormDesignation(e.target.value)}
               value={formDesignation}
             />
           </Form.Group>
         </Row>
 
-        
         <Row className="my-3 mx-auto">
           <Form.Group as={Col} controlId="formCompany">
             <Form.Label>Company:</Form.Label>
-            <Form.Control placeholder="Enter company name" 
-              onChange={(e)=> setFormCompany(e.target.value)}
+            <Form.Control
+              placeholder="Enter company name"
+              onChange={(e) => setFormCompany(e.target.value)}
               value={formCompany}
             />
           </Form.Group>
 
           <Form.Group as={Col} controlId="formInfo">
             <Form.Label>Information:</Form.Label>
-            <Form.Control placeholder="Enter customer information" 
-              onChange={(e)=> setFormInfo(e.target.value)}
+            <Form.Control
+              placeholder="Enter customer information"
+              onChange={(e) => setFormInfo(e.target.value)}
               value={formInfo}
             />
           </Form.Group>
@@ -134,38 +131,38 @@ export default function BasicForm() {
         <Row className="my-3 mx-auto">
           <Form.Group as={Col} controlId="photo">
             <Form.Label>Visiting Card Upload</Form.Label>
-            <Form.Control type='file' 
-              accept='.img, .jpg, .jpeg'
-              onChange={(e)=> handlePhoto(e)}
+            <Form.Control
+              type="file"
+              accept=".img, .jpg, .jpeg"
+              onChange={(e) => handlePhoto(e)}
             />
           </Form.Group>
-          
+
           <Form.Group as={Col} controlId="formInterestLebel">
-              <Form.Label>Interest Lebel</Form.Label>
-              <Form.Select aria-label="Default select example"
-                onChange={(e)=> setFormInterestLebel(e.target.value)}
-                value={formInterestLebel} 
-
-              >
-                  <option>Select Interest Level</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                  <option value="4">Four</option>
-                  <option value="5">Five</option>
-                </Form.Select>
-            </Form.Group>
+            <Form.Label>Interest Lebel</Form.Label>
+            <Form.Select
+              aria-label="Default select example"
+              onChange={(e) => setFormInterestLebel(e.target.value)}
+              value={formInterestLebel}
+            >
+              <option>Select Interest Level</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+              <option value="4">Four</option>
+              <option value="5">Five</option>
+            </Form.Select>
+          </Form.Group>
         </Row>
-
       </Card>
 
-      <div className='row justify-content-center my-3'>
-        <div className='col col-lg-2'>
-          <Button  type='submit' variant='outline-primary'>Submit</Button>
+      <div className="row justify-content-center my-3">
+        <div className="col col-lg-2">
+          <Button type="submit" variant="outline-primary">
+            Submit
+          </Button>
         </div>
       </div>
     </Form>
-
-  )
+  );
 }
-
--- Nitto_Clint_Side-main/src/Components/ShowData.jsx
+++ Nitto_Clint_Side-main/src/Components/ShowData.jsx
@@ -1,30 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 
-export default function ShowData(){
+export default function ShowData() {
+  const [customer, setCustomer] = useState([]);
 
-    const [customer, setCustomer] = useState([]);
+  useEffect(() => {
+    customerData();
+  }, []);
 
-    useEffect(() => {
-        customerData();
-    }, []);
-    
-
-    const customerData = async() =>{
-        const data = await fetch('http://localhost:5000/data',  {
-            method: 'GET',
-            body: JSON.stringify()
-        });
-        const allCustomer = await data.json()
-        //console.log(allCustomer)
-        setCustomer(allCustomer.allData)
-    }
-    console.log(customer);
-    return(
-        <Table striped bordered hover variant="danger" >
-           
-           
-            {/* {()=> (
+  const customerData = async () => {
+    const data = await fetch("http://localhost:5000/data", {
+      method: "GET",
+      body: JSON.stringify(),
+    });
+    const allCustomer = await data.json();
+    //console.log(allCustomer)
+    setCustomer(allCustomer.allData);
+  };
+  console.log(customer);
+  return (
+    <Table striped bordered hover variant="danger">
+      {/* {()=> (
                 customer.length() > 0 ? 
                 null
                 : 
@@ -43,36 +39,39 @@ export default function ShowData(){
                 </thead>)
             )}   */}
 
-            
-            
-            <thead>
-                <tr>
-                    <td>Name</td>
-                    <td>Phone</td>
-                    <td>Email</td>
-                    <td>Designation</td>
-                    <td>Company</td>
-                    <td>Information</td>
-                    <td>Interest Level</td>
-                    <td>Image</td>
-                    
-                </tr>
-            </thead>
-            <tbody>
-                {customer.map((val, index)=> (
-                    <tr key={index}>
-                        <td>{val.name}</td>
-                        <td>{val.phone}</td>
-                        <td>{val.email}</td>
-                        <td>{val.designation}</td>
-                        <td>{val.company}</td>
-                        <td>{val.info}</td>
-                        <td>{val.interest}</td>
-                        <td><img src={'http://localhost:5000/image/' + val.image} alt= "" /></td>
-                    </tr>
-                ))}
-            </tbody>
-            
-        </Table>
-    )
+      <thead>
+        <tr>
+          <td>Name</td>
+          <td>Phone</td>
+          <td>Email</td>
+          <td>Designation</td>
+          <td>Company</td>
+          <td>Information</td>
+          <td>Interest Level</td>
+          <td>Image</td>
+        </tr>
+      </thead>
+      <tbody>
+        {customer.map((val, index) => (
+          <tr key={index}>
+            <td>{val.name}</td>
+            <td>{val.phone}</td>
+            <td>{val.email}</td>
+            <td>{val.designation}</td>
+            <td>{val.company}</td>
+            <td>{val.info}</td>
+            <td>{val.interest}</td>
+            <td>
+              <img
+                height={60}
+                width={120}
+                src={"http://localhost:5000/image/" + val.image}
+                alt=""
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 }
